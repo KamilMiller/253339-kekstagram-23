69e7f425e1c5120ab22a3cdb71973d2b812a5668
Slow-match <boarsfield@gmail.com>
2021-05-29 12:47:53 +0300

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Функция получения случайных целых чисел.
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Функция для проверки максимальной длины строки.
+function checkLength(stroke, maxLength) {
+  return (stroke <= maxLength);
+}
+
+getRandomInt();
+checkLength();
